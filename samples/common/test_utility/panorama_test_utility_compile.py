ed12b1d5b74f10c1aacca627045a316ae85682b9
Tomonori Shimomura <shimomut@amazon.com>
2021-12-10 15:59:40 -0800

    - Enclose all the process of test-utility compile and run commands in a function, so that it doesn't contaminate the jupyter notebook namespace. - Added --output-pyplot option to simulate HDMI output by pyplot rendering on Jupyter notebook. - Renamed --screenshot-dir with --output-screenshots for consistency.

--- samples/common/test_utility/panorama_test_utility_compile.py
+++ samples/common/test_utility/panorama_test_utility_compile.py
@@ -1,81 +1,82 @@
 import sys
 import os
-import datetime
 import argparse
-
-import boto3
 
 import panorama_test_utility
 
 # ---
 
-argparser = argparse.ArgumentParser( description='Panorama Test-Utility compile command' )
-argparser.add_argument('--region', dest='region', action='store', default=None, help='Region name such as us-east-1')
-argparser.add_argument('--s3-model-location', dest='s3_model_location', action='store', required=True, help='S3 location for model compilation. e.g. s3://mybucket/myapp/')
-argparser.add_argument('--model-node-name', dest='model_node_names', action='append', required=True, help='Model node name')
-argparser.add_argument('--model-file-basename', dest='model_file_basenames', action='append', required=True, help='Model filename excluding .tar.gz part')
-argparser.add_argument('--model-data-shape', dest='model_data_shapes', action='append', required=True, help='Model input data shape. e.g. {"data":[1,3,512,512]}')
-argparser.add_argument('--model-framework', dest='model_frameworks', action='append', required=True, help='Model framework name. e.g. MXNET')
-args = argparser.parse_args()
+# Enclose all variables locally in this function, to avoid contaminating Jupyter notebook namespace when %run is used.
+def test_utility_compile_main():
 
-if len(args.model_node_names) != len(args.model_file_basenames) or len(args.model_node_names) != len(args.model_data_shapes) or len(args.model_node_names) != len(args.model_frameworks):
-    print( "Error: number of arguments have to be consistent between --model-node-name, --model-file-basenames, --model-data-shape, and --model-framework" )
-    sys.exit(1)
+    argparser = argparse.ArgumentParser( description='Panorama Test-Utility compile command' )
+    argparser.add_argument('--region', dest='region', action='store', default=None, help='Region name such as us-east-1')
+    argparser.add_argument('--s3-model-location', dest='s3_model_location', action='store', required=True, help='S3 location for model compilation. e.g. s3://mybucket/myapp/')
+    argparser.add_argument('--model-node-name', dest='model_node_names', action='append', required=True, help='Model node name')
+    argparser.add_argument('--model-file-basename', dest='model_file_basenames', action='append', required=True, help='Model filename excluding .tar.gz part')
+    argparser.add_argument('--model-data-shape', dest='model_data_shapes', action='append', required=True, help='Model input data shape. e.g. {"data":[1,3,512,512]}')
+    argparser.add_argument('--model-framework', dest='model_frameworks', action='append', required=True, help='Model framework name. e.g. MXNET')
+    args = argparser.parse_args()
 
-# ---
+    if len(args.model_node_names) != len(args.model_file_basenames) or len(args.model_node_names) != len(args.model_data_shapes) or len(args.model_node_names) != len(args.model_frameworks):
+        print( "Error: number of arguments have to be consistent between --model-node-name, --model-file-basenames, --model-data-shape, and --model-framework" )
+        sys.exit(1)
 
-c = panorama_test_utility.Config()
+    # slash character at the end of S3 prefix is optional.
+    args.s3_model_location = args.s3_model_location.rstrip("/")
 
-# ---
+    # ---
 
-def compile_model_as_needed( model_node_name, model_file_basename, model_data_shape, model_framework ):
+    c = panorama_test_utility.Config()
 
-    print( f"Checking [{model_node_name}]" )
+    # ---
 
-    model_file_dirname, model_file_basename = os.path.split(model_file_basename)
+    def compile_model_as_needed( model_node_name, model_file_basename, model_data_shape, model_framework ):
 
-    raw_model_file = f"{model_file_dirname}/{model_file_basename}.tar.gz"
-    compiled_model_dir = f"{model_file_dirname}/{model_node_name}"
-    compiled_model_file = f"{compiled_model_dir}/{model_file_basename}-{c.compiled_model_suffix}.tar.gz"
+        print( f"Checking [{model_node_name}]" )
 
-    need_model_compilation = False
+        model_file_dirname, model_file_basename = os.path.split(model_file_basename)
 
-    if not os.path.exists(raw_model_file):
-        print( f"Error : Raw model file [{raw_model_file}] doesn't exist" )
-        return
+        raw_model_file = f"{model_file_dirname}/{model_file_basename}.tar.gz"
+        compiled_model_dir = f"{model_file_dirname}/{model_node_name}"
+        compiled_model_file = f"{compiled_model_dir}/{model_file_basename}-{c.compiled_model_suffix}.tar.gz"
 
-    elif not os.path.exists(compiled_model_file):
-        print( f"Compiled model file [{compiled_model_file}] doesn't exist. Need compilation." )
-        need_model_compilation = True
+        need_model_compilation = False
 
-    else:
-        raw_model_stat = os.stat( raw_model_file )
-        compiled_model_stat = os.stat( compiled_model_file )
-    
-        if raw_model_stat.st_mtime > compiled_model_stat.st_mtime:
-            print( "Raw model file has newer timestamp than compiled model file. Need compilation." )
+        if not os.path.exists(raw_model_file):
+            print( f"Error : Raw model file [{raw_model_file}] doesn't exist" )
+            return
+
+        elif not os.path.exists(compiled_model_file):
+            print( f"Compiled model file [{compiled_model_file}] doesn't exist. Need compilation." )
             need_model_compilation = True
+
         else:
-            print( "Compiled model file is up to date. Skipping compilation." )
-
-    if need_model_compilation:
-        panorama_test_utility.prepare_model_for_test(
-            region = args.region,
-            data_shape = model_data_shape,
-            framework = model_framework,
-            input_model_filepath = raw_model_file,
-            output_model_dir = compiled_model_dir,
-            s3_model_location = args.s3_model_location + "/" + model_node_name,
-            compile_job_role = panorama_test_utility.resolve_sm_role(),
-        )
-
-def main():
+            raw_model_stat = os.stat( raw_model_file )
+            compiled_model_stat = os.stat( compiled_model_file )
     
+            if raw_model_stat.st_mtime > compiled_model_stat.st_mtime:
+                print( "Raw model file has newer timestamp than compiled model file. Need compilation." )
+                need_model_compilation = True
+            else:
+                print( "Compiled model file is up to date. Skipping compilation." )
+
+        if need_model_compilation:
+            panorama_test_utility.prepare_model_for_test(
+                region = args.region,
+                data_shape = model_data_shape,
+                framework = model_framework,
+                input_model_filepath = raw_model_file,
+                output_model_dir = compiled_model_dir,
+                s3_model_location = args.s3_model_location + "/" + model_node_name,
+                compile_job_role = panorama_test_utility.resolve_sm_role(),
+            )
+
     panorama_test_utility.configure(c)
-    
+
     # TODO : parallelize
     for model_node_name, model_file_basename, model_data_shape, model_framework in zip( args.model_node_names, args.model_file_basenames, args.model_data_shapes, args.model_frameworks ):
         compile_model_as_needed( model_node_name, model_file_basename, model_data_shape, model_framework )
 
-main()
+test_utility_compile_main()
 
--- samples/common/test_utility/panorama_test_utility_run.py
+++ samples/common/test_utility/panorama_test_utility_run.py
@@ -9,84 +9,105 @@ import panorama_test_utility
 
 # ---
 
-argparser = argparse.ArgumentParser( description='Panorama Test-Utility' )
-argparser.add_argument('--region', dest='region', action='store', default=None, help='Region name such as us-east-1')
-argparser.add_argument('--app-name', dest='app_name', action='store', required=True, help='Application name')
-argparser.add_argument('--code-package-name', dest='code_package_name', required=True, action='store', help='Code package name')
-argparser.add_argument('--model-package-name', dest='model_package_name', required=True, action='store', help='Model package name')
-argparser.add_argument('--camera-node-name', dest='camera_node_name', required=True, action='store', help='Camera node name')
-argparser.add_argument('--model-node-name', dest='model_node_names', action='append', required=True, help='Model node name')
-argparser.add_argument('--model-file-basename', dest='model_file_basenames', action='append', required=True, help='Model filename excluding .tar.gz part')
-argparser.add_argument('--video-file', dest='video_file', action='store', required=True, help='Video filename to simulate camera stream')
-argparser.add_argument('--video-start', dest='video_start', action='store', default=0, help='Video start frame (default: 0)')
-argparser.add_argument('--video-stop', dest='video_stop', action='store', default=30, help='Video stop frame (default: 30)')
-argparser.add_argument('--video-step', dest='video_step', action='store', default=1, help='Video frame step (default: 1)')
-argparser.add_argument('--screenshot-dir', dest='screenshot_dir', action='store', default=None, help="Directory name to save screenshot files. You can use Python's datetime format.")
-argparser.add_argument('--py-file', dest='py_file', action='store', required=True, help='Python source path to execute')
-args = argparser.parse_args()
+# Enclose all variables locally in this function, to avoid contaminating Jupyter notebook namespace when %run is used.
+def test_utility_run_main():
 
-if len(args.model_node_names) != len(args.model_file_basenames):
-    print( "Error: number of arguments have to be consistent between --model-node-name and --model-file-basenames" )
-    sys.exit(1)
+    argparser = argparse.ArgumentParser( description='Panorama Test-Utility' )
+    argparser.add_argument('--region', dest='region', action='store', default=None, help='Region name such as us-east-1')
+    argparser.add_argument('--app-name', dest='app_name', action='store', required=True, help='Application name')
+    argparser.add_argument('--code-package-name', dest='code_package_name', required=True, action='store', help='Code package name')
+    argparser.add_argument('--model-package-name', dest='model_package_name', required=True, action='store', help='Model package name')
+    argparser.add_argument('--camera-node-name', dest='camera_node_name', required=True, action='store', help='Camera node name')
+    argparser.add_argument('--model-node-name', dest='model_node_names', action='append', required=True, help='Model node name')
+    argparser.add_argument('--model-file-basename', dest='model_file_basenames', action='append', required=True, help='Model filename excluding .tar.gz part')
+    argparser.add_argument('--video-file', dest='video_file', action='store', required=True, help='Video filename to simulate camera stream')
+    argparser.add_argument('--video-start', dest='video_start', action='store', default=0, help='Video start frame (default: 0)')
+    argparser.add_argument('--video-stop', dest='video_stop', action='store', default=30, help='Video stop frame (default: 30)')
+    argparser.add_argument('--video-step', dest='video_step', action='store', default=1, help='Video frame step (default: 1)')
+    argparser.add_argument('--output-pyplot', dest='output_pyplot', action='store_true', default=False, help="Simulate HDMI output by rendering on Jupyter notebook with pyplot.")
+    argparser.add_argument('--output-screenshots', dest='output_screenshots', action='store', default=None, help="Simulate HDMI output by generating sequentially numbered PNG files. Directory name has to be specified. You can use Python's datetime format.")
+    argparser.add_argument('--py-file', dest='py_file', action='store', required=True, help='Python source path to execute')
+    args = argparser.parse_args()
 
-# ---
+    if len(args.model_node_names) != len(args.model_file_basenames):
+        print( "Error: number of arguments have to be consistent between --model-node-name and --model-file-basenames" )
+        sys.exit(1)
 
-model_node_and_file = {}
-for model_node_name, model_file_basename in zip( args.model_node_names, args.model_file_basenames):
-    model_file_dirname, model_file_basename = os.path.split(model_file_basename)
-    compiled_model_basename = f"{model_file_dirname}/{model_node_name}/{model_file_basename}"
-    model_node_and_file[model_node_name] = compiled_model_basename
+    # ---
 
-screenshot_dir_dt_resolved = None
-if args.screenshot_dir:
-    screenshot_dir_dt_resolved = datetime.datetime.now().strftime(args.screenshot_dir)
-    os.makedirs( screenshot_dir_dt_resolved, exist_ok=True )
+    model_node_and_file = {}
+    for model_node_name, model_file_basename in zip( args.model_node_names, args.model_file_basenames):
+        model_file_dirname, model_file_basename = os.path.split(model_file_basename)
+        compiled_model_basename = f"{model_file_dirname}/{model_node_name}/{model_file_basename}"
+        model_node_and_file[model_node_name] = compiled_model_basename
 
-c = panorama_test_utility.Config(
+    screenshot_dir_dt_resolved = None
+    if args.output_screenshots:
+        screenshot_dir_dt_resolved = datetime.datetime.now().strftime(args.output_screenshots)
+        os.makedirs( screenshot_dir_dt_resolved, exist_ok=True )
 
-    # application name
-    app_name = args.app_name,
+    c = panorama_test_utility.Config(
 
-    ## package names and node names
-    code_package_name = args.code_package_name,
-    model_package_name = args.model_package_name,
-    camera_node_name = args.camera_node_name,
+        # application name
+        app_name = args.app_name,
 
-    # models (model node name : compiled model path without platform dependent suffic)
-    models = model_node_and_file,
+        ## package names and node names
+        code_package_name = args.code_package_name,
+        model_package_name = args.model_package_name,
+        camera_node_name = args.camera_node_name,
 
-    # video file path to simulate camera stream
-    videoname = args.video_file,
-    video_range = range( int(args.video_start), int(args.video_stop), int(args.video_step) ),
+        # models (model node name : compiled model path without platform dependent suffic)
+        models = model_node_and_file,
+
+        # video file path to simulate camera stream
+        videoname = args.video_file,
+        video_range = range( int(args.video_start), int(args.video_stop), int(args.video_step) ),
     
-    # Suppress rendering output by pyplot, and write screenshots in PNG files
-    render_output_image_with_pyplot = False,
-    screenshot_dir = screenshot_dir_dt_resolved,
+        # Suppress rendering output by pyplot, and write screenshots in PNG files
+        render_output_image_with_pyplot = args.output_pyplot,
+        screenshot_dir = screenshot_dir_dt_resolved,
 
-    # AWS account ID
-    account_id = boto3.client("sts").get_caller_identity()["Account"],
-)
+        # AWS account ID
+        account_id = boto3.client("sts").get_caller_identity()["Account"],
+    )
 
-# ---
+    # ---
 
-def run_simulation():
+    class NullStdout:
+        def write(self,s):
+            pass
+        def flush(self):
+            pass
 
-    name = os.path.basename(args.py_file)
+    def run_simulation():
 
-    with open( args.py_file ) as fd:
-        file_image = fd.read()
+        name = os.path.basename(args.py_file)
 
-    try:
-        namespace = {}
-        code = compile( file_image, name, 'exec' )
-        exec( code, namespace, namespace )
-    except panorama_test_utility.panoramasdk.TestUtilityEndOfVideo:
-        print( "Reached end of video. Stopped simulation." )
+        with open( args.py_file ) as fd:
+            file_image = fd.read()
 
-def main():
-    
+        # When --output-pyplot option is specified, suppress stdout in order not to wipe rendered image
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        if args.output_pyplot:
+            sys.stdout = NullStdout()
+            sys.stderr = NullStdout()
+
+        try:
+            namespace = {}
+        
+            code = compile( file_image, name, 'exec' )
+            exec( code, namespace, namespace )
+            
+        except panorama_test_utility.panoramasdk.TestUtilityEndOfVideo:
+            print( "Reached end of video. Stopped simulation." )
+
+        finally:
+            sys.stdout = original_stdout
+            sys.stderr = original_stderr
+
     panorama_test_utility.configure(c)
-    
     run_simulation()
 
-main()
+test_utility_run_main()
+
