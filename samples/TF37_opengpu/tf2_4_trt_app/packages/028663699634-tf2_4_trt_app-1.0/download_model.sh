5a290e9f2d92274f4da7e36b2eb001ec886cff1d
Kevin Hsu <kevhsu@amazon.com>
2022-08-15 06:35:23 +0000

    feat: move code back to git

---
+++ samples/TF37_opengpu/tf2_4_trt_app/packages/028663699634-tf2_4_trt_app-1.0/download_model.sh
@@ -0,0 +1,3 @@
+wget https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TF_Sample/Model/saved_model_trt_fp16_tf27.zip
+unzip saved_model_trt_fp16_tf27.zip
+rm saved_model_trt_fp16_tf27.zip
