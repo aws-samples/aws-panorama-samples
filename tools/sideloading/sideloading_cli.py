c1da774a9cca19b0411863f11ebbe208902af906
Tomonori Shimomura <shimomut@amazon.com>
2022-10-15 00:37:02 +0000

    Initial commit of sideloading tool

---
+++ tools/sideloading/sideloading_cli.py
@@ -0,0 +1,286 @@
+import sys
+import os
+import json
+import urllib
+import pprint
+import datetime
+import http.client
+import ssl
+import argparse
+
+
+class SideloadingClient:
+
+    def __init__( self, server_address, port, src_top ):
+    
+        self.src_top = os.path.normpath(src_top)
+
+        self.ssl_context = ssl.create_default_context( purpose=ssl.Purpose.SERVER_AUTH )
+
+        self.ssl_context.check_hostname = False
+        self.ssl_context.verify_mode = ssl.CERT_REQUIRED
+
+        # Server certificate
+        self.ssl_context.load_verify_locations( "./certs_keys/server.pem.cert" )
+
+        # Client certificate & secret-key
+        self.ssl_context.load_cert_chain( "./certs_keys/client.pem.cert", "./certs_keys/client.pem.key" )
+
+        self.https_conn = http.client.HTTPSConnection( server_address, port, context=self.ssl_context)
+
+    def sendFile( self, src_filename ):
+    
+        src_filename = src_filename.replace("\\","/")
+
+        src_fullpath = os.path.join( self.src_top, src_filename )
+        src_fullpath = os.path.normpath(src_fullpath).replace("\\","/")
+        assert src_fullpath.startswith(self.src_top)
+        
+        st = os.stat(src_fullpath)
+        dt_mtime = datetime.datetime.fromtimestamp( st.st_mtime )
+        
+        with open(src_fullpath,"rb") as src_fd:
+            d = src_fd.read()
+        
+        headers = {
+            "mtime" : dt_mtime.isoformat()
+        }
+        
+        print( f"Sending {src_filename}" )
+        
+        self.https_conn.request( 'PUT', '/files/' + urllib.parse.quote(src_filename), body = d, headers=headers )
+
+        response = self.https_conn.getresponse()
+        d = json.loads(response.read())
+        return d
+
+    def deleteFile( self, src_filename ):
+    
+        src_filename = src_filename.replace("\\","/")
+
+        print( f"Deleting {src_filename}" )
+
+        self.https_conn.request( 'DELETE', '/files/' + urllib.parse.quote(src_filename) )
+
+        response = self.https_conn.getresponse()
+        d = json.loads(response.read())
+        return d
+
+    def listFiles( self ):
+    
+        self.https_conn.request( 'GET', '/files' )
+
+        response = self.https_conn.getresponse()
+        d = json.loads(response.read())
+        return d
+
+    def sync(self):
+        
+        # List all files in destination
+        dst_files_list = self.listFiles()
+
+        # List all files in source
+        src_files_list = []
+        for place, dirs, files in os.walk( self.src_top ):
+            for filename in files:
+                
+                filepath = os.path.join( place, filename ).replace("\\","/")
+                assert filepath.startswith(self.src_top)
+                filepath_relative = filepath[ len(self.src_top) : ]
+                filepath_relative = filepath_relative.lstrip("/\\")
+                filepath_relative = filepath_relative.replace("\\","/")
+                
+                st = os.stat(filepath)
+                dt_mtime = datetime.datetime.fromtimestamp( st.st_mtime )
+                
+                src_files_list.append( {
+                    "filepath" : filepath_relative,
+                    "mtime" : dt_mtime.isoformat(),
+                    "size" : st.st_size,
+                })
+        
+        src_files_table = {}
+        for src_file in src_files_list:
+            src_files_table[ src_file["filepath"] ] = src_file
+
+        dst_files_table = {}
+        for dst_file in dst_files_list:
+            dst_files_table[ dst_file["filepath"] ] = dst_file
+        
+        # Copy all files            
+        for src_file in src_files_list:
+            
+            filepath = src_file["filepath"]
+            
+            if filepath in dst_files_table:
+                
+                dst_file = dst_files_table[filepath]
+                
+                src_mtime = datetime.datetime.fromisoformat( src_file["mtime"] ).timestamp()
+                dst_mtime = datetime.datetime.fromisoformat( dst_file["mtime"] ).timestamp()
+
+                # Skip unchanged file
+                if abs(src_mtime - dst_mtime)<1 and src_file["size"]==dst_file["size"]:
+                    print( f"Unchanged {filepath}" )
+                    continue
+                
+            # Send changed file
+            self.sendFile( filepath )
+                
+        # Delete files which disappeard in source
+        for dst_file in dst_files_list:
+            
+            filepath = dst_file["filepath"]
+            
+            if filepath not in src_files_table:
+                self.deleteFile(filepath)
+        
+        return {}
+
+    def runApplication(self):
+    
+        self.https_conn.request( 'POST', '/application' )
+
+        response = self.https_conn.getresponse()
+        d = json.loads(response.read())
+        return d
+
+    def killApplication(self):
+    
+        self.https_conn.request( 'DELETE', '/application' )
+
+        response = self.https_conn.getresponse()
+        d = json.loads(response.read())
+        return d
+
+
+# ---
+
+
+def send_file( argv ):
+
+    argparser = argparse.ArgumentParser( description='Send single file' )
+    argparser.add_argument('--addr', dest='addr', action='store', required=True, help='IP address or hostname of Panorama device')
+    argparser.add_argument('--port', dest='port', action='store', default=8123, help='Port number (default:8123)')
+    argparser.add_argument('filepath', action='store', type=str, help='Relative filepath ')
+    args = argparser.parse_args( argv )
+    
+    sideloading_client = SideloadingClient(
+        server_address = args.addr,
+        port = args.port,
+        src_top = "./src",
+    )
+
+    sideloading_client.sendFile( args.filepath )
+    
+
+def delete_file( argv ):
+
+    argparser = argparse.ArgumentParser( description='Delete single file' )
+    argparser.add_argument('--addr', dest='addr', action='store', required=True, help='IP address or hostname of Panorama device')
+    argparser.add_argument('--port', dest='port', action='store', default=8123, help='Port number (default:8123)')
+    argparser.add_argument('filepath', action='store', type=str, help='Relative filepath ')
+    args = argparser.parse_args( argv )
+    
+    sideloading_client = SideloadingClient(
+        server_address = args.addr,
+        port = args.port,
+        src_top = "./src",
+    )
+
+    sideloading_client.deleteFile( args.filepath )
+    
+
+def list_files( argv ):
+
+    argparser = argparse.ArgumentParser( description='List all sideloaded files' )
+    argparser.add_argument('--addr', dest='addr', action='store', required=True, help='IP address or hostname of Panorama device')
+    argparser.add_argument('--port', dest='port', action='store', default=8123, help='Port number (default:8123)')
+    args = argparser.parse_args( argv )
+    
+    sideloading_client = SideloadingClient(
+        server_address = args.addr,
+        port = args.port,
+        src_top = "./src",
+    )
+
+    files = sideloading_client.listFiles()
+    
+    max_filepath_length = 10
+    max_mtime_length = 10
+    for f in files:
+        max_filepath_length = max( len(f["filepath"]), max_filepath_length )
+        max_mtime_length = max( len(f["mtime"]), max_mtime_length )
+
+    format = f"%{max_filepath_length}s  %-{max_mtime_length}s  %s"
+    print( format % ("filepath", "mtime", "size") )
+    for f in files:
+        print( format % (f["filepath"], f["mtime"], f["size"]) )
+    
+
+def sync( argv ):
+
+    argparser = argparse.ArgumentParser( description='List all sideloaded files' )
+    argparser.add_argument('--addr', dest='addr', action='store', required=True, help='IP address or hostname of Panorama device')
+    argparser.add_argument('--port', dest='port', action='store', default=8123, help='Port number (default:8123)')
+    args = argparser.parse_args( argv )
+    
+    sideloading_client = SideloadingClient(
+        server_address = args.addr,
+        port = args.port,
+        src_top = "./src",
+    )
+
+    sideloading_client.sync()
+    
+    
+def run_app( argv ):
+
+    argparser = argparse.ArgumentParser( description='List all sideloaded files' )
+    argparser.add_argument('--addr', dest='addr', action='store', required=True, help='IP address or hostname of Panorama device')
+    argparser.add_argument('--port', dest='port', action='store', default=8123, help='Port number (default:8123)')
+    args = argparser.parse_args( argv )
+    
+    sideloading_client = SideloadingClient(
+        server_address = args.addr,
+        port = args.port,
+        src_top = "./src",
+    )
+
+    sideloading_client.runApplication()
+    
+
+def kill_app( argv ):
+
+    argparser = argparse.ArgumentParser( description='List all sideloaded files' )
+    argparser.add_argument('--addr', dest='addr', action='store', required=True, help='IP address or hostname of Panorama device')
+    argparser.add_argument('--port', dest='port', action='store', default=8123, help='Port number (default:8123)')
+    args = argparser.parse_args( argv )
+    
+    sideloading_client = SideloadingClient(
+        server_address = args.addr,
+        port = args.port,
+        src_top = "./src",
+    )
+
+    sideloading_client.killApplication()
+    
+
+command_table = {
+    "send-file" : send_file,
+    "delete-file" : delete_file,
+    "list-files" : list_files,
+    "sync" : sync,
+    "run-app" : run_app,
+    "kill-app" : kill_app,
+}
+
+
+argparser = argparse.ArgumentParser( description = 'Panorama sideloading commands' )
+command_names = ", ".join( command_table.keys() ).strip(" ,") 
+argparser.add_argument('command', help=f'Command to run ( {command_names} )')
+args = argparser.parse_args( sys.argv[1:2] )
+
+command_func = command_table[args.command]
+command_func( sys.argv[2:] )
+
