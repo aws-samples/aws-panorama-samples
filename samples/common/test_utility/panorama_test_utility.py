896061f495fef3eade2600d285d4010894244663
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 09:23:03 +0000

    Adding tarfile member sanitization to extractall()

--- samples/common/test_utility/panorama_test_utility.py
+++ samples/common/test_utility/panorama_test_utility.py
@@ -105,7 +105,26 @@ def split_s3_path( s3_path ):
 
 def extract_targz( targz_filename, dst_dirname ):
     with tarfile.open( targz_filename, "r" ) as tar_fd:
-        tar_fd.extractall( path = dst_dirname )
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar_fd, path=dst_dirname)
 
 
 def resolve_sm_role():
