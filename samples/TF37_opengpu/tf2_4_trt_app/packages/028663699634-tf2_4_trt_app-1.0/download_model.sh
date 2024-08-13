cb4fd0d55302fdfde5d6b4c44c3f7de036375b5d
Eric Liu <liuri@amazon.com>
2024-08-13 19:54:11 +0000

    Remove sample applications incompatible with Jetpack 5

--- samples/TF37_opengpu/tf2_4_trt_app/packages/028663699634-tf2_4_trt_app-1.0/download_model.sh
+++
@@ -1,3 +0,0 @@
-wget https://panorama-starter-kit.s3.amazonaws.com/public/v2/opengpusamples/TF_Sample/Model/saved_model_trt_fp16.zip
-unzip saved_model_trt_fp16.zip
-rm saved_model_trt_fp16.zip
--- samples/pose_estimation_opengpu/pose_estimation_app/packages/123456789012-pose_estimation_code-1.0/src/run_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu/tegra:/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
-export OPENBLAS_CORETYPE=ARMV8
-python3 /panorama/app.py
